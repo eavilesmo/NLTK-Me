317b67f213c4ee52e28671e8f531e600de57dc55
Esperanza <esperanza.aviles@clarity.ai>
2022-11-01 12:06:24 +0100

    test: test_tokenize_text passed

--- test/nltk_me_test.py
+++ test/nltk_me_test.py
@@ -1,11 +1,19 @@
 import unittest
-from src.Text_Processing_Tools import tokenize_text
+from doubles import allow
+
+from src.TextProcessingTools import TextProcessingTools
 
 
 class TestNltkApp(unittest.TestCase):
 
     def test_tokenize_text(self):
-        self.assertEqual(True, True)
+        get_file_path = ""
+        filename_path = ""
+        text_processing_tools = TextProcessingTools()
+
+        allow(text_processing_tools).tokenize_text(get_file_path, filename_path).and_return("Hello")
+
+        self.assertEqual(text_processing_tools.tokenize_text(get_file_path, filename_path), "Hello")
 
 
 if __name__ == '__main__':
