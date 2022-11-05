8e98320b9afb4f7bc303b63b4a9c8fae4fcd195b
Esperanza <esperanza.aviles@clarity.ai>
2022-11-05 12:08:16 +0100

    test: test_tokenize_text passed

--- test/nltk_me_test.py
+++ test/nltk_me_test.py
@@ -1,21 +1,27 @@
 import unittest
-from doubles import allow
+from doublex import Stub, assert_that
+from hamcrest import equal_to, is_
 
+from src.FileHandler import FileHandler
 from src.TextProcessingTools import TextProcessingTools
 
 
 class TestNltkApp(unittest.TestCase):
 
     def test_tokenize_text(self):
-        get_file_path = ""
-        filename_path = ""
+        file_content = "This is a sentence. This is a second sentence."
+        get_file_path = "any_path"
+        filename_path = "any_file_name_path"
+        expected_result = "This is a sentence.\nThis is a second sentence."
         text_processing_tools = TextProcessingTools()
-        expected_result = "Hello"
-        allow(text_processing_tools).tokenize_text(get_file_path, filename_path).and_return("Hello")
+
+        with Stub(FileHandler) as stub:
+            stub.readFile(get_file_path).returns(file_content)
+            stub.write(filename_path, expected_result)
 
         actual_result = text_processing_tools.tokenize_text(get_file_path, filename_path)
 
-        self.assertEqual(actual_result, expected_result)
+        assert_that(actual_result), is_(expected_result)
 
 
 if __name__ == '__main__':
