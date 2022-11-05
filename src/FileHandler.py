2aed2404817d15dbfcea51e4cc9ea11fa09fe18e
Esperanza <esperanza.aviles@clarity.ai>
2022-11-05 13:54:31 +0100

    feat: created writeStopwords method in FileHandler

--- src/FileHandler.py
+++ src/FileHandler.py
@@ -8,3 +8,10 @@ class FileHandler:
             for line in tokenized_text:
                 result_text.write(line)
                 result_text.write("\n")
+
+    def writeStopwords(self, filename_path, tokenized_file, stop_words, stopword_symbols):
+        with open(filename_path, "w") as result_stopwords:
+            for word in tokenized_file:
+                word = word.lower()
+                if word not in stop_words and word not in stopword_symbols:
+                    result_stopwords.write(" " + word)
