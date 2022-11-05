9ed828a2efa7fed099f993d0cea64182c5cee5d5
Esperanza <esperanza.aviles@clarity.ai>
2022-11-05 13:38:11 +0100

    test: test_tokenize_sentence passed

--- test/nltk_me_test.py
+++ test/nltk_me_test.py
@@ -15,27 +15,26 @@ class TestNltkApp(unittest.TestCase):
         expected_result = ["This is a sentence.", "This is a second sentence."]
 
         with Stub(FileHandler) as stub:
-            when(stub).readFile(get_file_path).returns(file_content)
-            stub.write(filename_path, expected_result)
+            when(stub).readFile(ANY_ARG).returns(file_content)
+            stub.write(ANY_ARG)
             text_processing_tools = TextProcessingTools(stub)
             actual_result = text_processing_tools.tokenize_text(get_file_path, filename_path)
 
         assert_that(actual_result, equal_to(expected_result))
 
-    # def test_tokenize_sentence(self):
-    #     file_content = "This is a sentence."
-    #     get_file_path = "any_path"
-    #     filename_path = "any_file_name_path"
-    #     expected_tokenized_sentence = "This\nis\na\nsentence."
-    #     text_processing_tools = TextProcessingTools()
-    #
-    #     with Stub(FileHandler) as stub:
-    #         stub.readFile(get_file_path).returns(file_content)
-    #         stub.write(filename_path, expected_tokenized_sentence)
-    #
-    #     actual_tokenized_sentence = text_processing_tools.tokenize_sentence(get_file_path, filename_path)
-    #
-    #     assert_that(actual_tokenized_sentence, equal_to(expected_tokenized_sentence))
+    def test_tokenize_sentence(self):
+        file_content = "This is a sentence."
+        get_file_path = "any_path"
+        filename_path = "any_file_name_path"
+        expected_tokenized_sentence = ["This", "is", "a", "sentence", "."]
+
+        with Stub(FileHandler) as stub:
+            when(stub).readFile(ANY_ARG).returns(file_content)
+            stub.write(ANY_ARG)
+            text_processing_tools = TextProcessingTools(stub)
+            actual_tokenized_sentence = text_processing_tools.tokenize_sentence(get_file_path, filename_path)
+
+        assert_that(actual_tokenized_sentence, equal_to(expected_tokenized_sentence))
 
 
 if __name__ == '__main__':
