db510062e0349e2cfa6fb2c5ba2b99ecba3df39d
Esperanza <esperanza.aviles@clarity.ai>
2022-11-14 19:35:44 +0100

    refactor: rename class to TextProcessingToolsTest

--- test/nltk_me_test.py
+++ test/nltk_me_test.py
@@ -6,7 +6,7 @@ from src.infrastructure.FileHandler import FileHandler
 from src.domain.TextProcessingTools import TextProcessingTools
 
 
-class TestNltkApp(unittest.TestCase):
+class TextProcessingToolsTest(unittest.TestCase):
 
     def test_tokenize_text(self):
         file_content = "This is a sentence. This is a second sentence."
