0b24e8b1f1daca24e22ea829eacdcbef8c89da48
Esperanza <esperanza.aviles@clarity.ai>
2022-11-17 20:52:19 +0100

    refactor: rename Executor methods

--- src/infrastructure/Executor.py
+++ src/infrastructure/Executor.py
@@ -27,19 +27,19 @@ class Executor:
             return "The file is empty."
         self.file_handler.write(filename_path, processed_content)
 
-    def total_words_count(self, file):
+    def count_total_words(self, file):
         content_to_process = self.file_handler.read_file(file)
         total_words = self.text_processing_tools.count_total_words(content_to_process)
         if len(total_words) <= 0:
             return "The file is empty."
 
-    def freqdist_count(self, file, word_to_search_for):
+    def count_repetitions_of_a_word(self, file, word_to_search_for):
         content_to_process = self.file_handler.read_file(file)
         number_of_repetitions = self.text_processing_tools.count_repetitions_of_a_word(content_to_process, word_to_search_for)
         if len(number_of_repetitions) <= 0:
             return "The file is empty."
 
-    def freqdist_max(self, file):
+    def find_most_repeated_word(self, file):
         content_to_process = self.file_handler.read_file(file)
         most_repeated_word = self.text_processing_tools.find_most_repeated_word(content_to_process)
         if len(most_repeated_word) <= 0:
