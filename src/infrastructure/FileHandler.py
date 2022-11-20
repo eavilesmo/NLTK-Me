8df01afb250b8ae47aa3eb69b411dd8eb56c784f
Esperanza <esperanza.aviles@clarity.ai>
2022-11-20 17:11:48 +0100

    refactor: improve naming in methods and variables from FileHandler file

--- src/infrastructure/FileHandler.py
+++ src/infrastructure/FileHandler.py
@@ -1,17 +1,17 @@
 class FileHandler:
 
-    def read_file(self, get_file_text):
-        return open(get_file_text, encoding="UTF-8").read()
+    def read(self, input_file):
+        return open(input_file, encoding="UTF-8").read()
 
-    def write(self, filename_path, tokenized_text):
-        with open(filename_path, "w") as result_text:
-            for line in tokenized_text:
-                result_text.write(line)
-                result_text.write("\n")
+    def write(self, file_path, processed_content):
+        with open(file_path, "w") as output_file:
+            for line in processed_content:
+                output_file.write(line)
+                output_file.write("\n")
 
-    def write_stopwords(self, filename_path, tokenized_file, stop_words, stopword_symbols):
-        with open(filename_path, "w") as result_stopwords:
-            for word in tokenized_file:
+    def write_stopwords(self, file_path, processed_content, stopwords, symbols):
+        with open(file_path, "w") as output_file:
+            for word in processed_content:
                 word = word.lower()
-                if word not in stop_words and word not in stopword_symbols:
-                    result_stopwords.write(" " + word)
+                if word not in stopwords and word not in symbols:
+                    output_file.write(" " + word)
