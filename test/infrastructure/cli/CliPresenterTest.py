00e892245b0ce3709e164fb0e03793fdaef81d08
Esperanza <esperanza.aviles@clarity.ai>
2022-12-20 20:27:09 +0100

    test: create test_show_introduction in CliPresenterTest

--- test/infrastructure/cli/CliPresenterTest.py
+++ test/infrastructure/cli/CliPresenterTest.py
@@ -1,6 +1,12 @@
-from unittest import TestCase
+from src.infrastructure.cli.CliPresenter import CliPresenter
+
+cli_presenter = CliPresenter()
 
 
-class CliPresenterTest(TestCase):
-    def test_show_options(self):
-        self.fail()
+def test_show_introduction(capsys):
+    cli_presenter.show_introduction()
+    captured = capsys.readouterr()
+    expected_output = "Welcome to NLTK-Me!\nWhat would you like to do?"
+    assert captured.out == expected_output
+
+
